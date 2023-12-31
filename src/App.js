79b068c37bb8dce6aefb1bf909c70ff7d7c32f59
Михаил Недиков <nedikov.misha@yandex.ru>
2024-01-01 03:12:05 +0400

    [add]: html and css for player

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Player from './components/player';
+
+function App() {
+  return (
+    
+    <Player />
+   
+  );
+}
+
+export default App;
